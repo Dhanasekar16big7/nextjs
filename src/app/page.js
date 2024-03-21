76b15c2721f51dbb1193a31af0f1f6b7ecbeaf8a
Dhanasekar <Dhanasekar16big7>
2024-03-21 15:19:55 +0530

    changes done

--- src/app/page.js
+++ src/app/page.js
@@ -4,8 +4,10 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center gap-12 p-24">
       <h1 className="text-5xl font-bold">Home page</h1>
-      <Image src="/user-image.png" alt="Vercel Logo" width={150} height={150} />
-      <p className="text-3xl font-bold">Hello world!</p>
+      <div className="card_data flex flex-col items-center gap-5 shadow-md p-4 rounded w-1/2 text-center bg-white">
+        <Image src="/user-image.png" alt="Vercel Logo" width={150} height={150} />
+        <p className="text-3xl font-bold">Hello world!</p>
+      </div>
     </main>
   );
 }
