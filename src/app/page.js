e45cd6d81ff8632d659a6d2b1417b2390b8edc76
Dhanasekar <Dhanasekar16big7>
2024-03-21 15:27:25 +0530

    changes done

--- src/app/page.js
+++ src/app/page.js
@@ -3,10 +3,10 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center gap-12 p-24">
-      <h1 className="text-5xl font-bold">Home page</h1>
+      <h1 className="text-4xl font-bold text-center">Home page</h1>
       <div className="card_data flex flex-col items-center gap-5 shadow-md p-4 rounded w-1/2 text-center bg-white">
         <Image src="/user-image.png" alt="Vercel Logo" width={150} height={150} />
-        <p className="text-3xl font-bold">Hello world!</p>
+        <p className="text-2xl font-bold">Hello world!</p>
       </div>
     </main>
   );
