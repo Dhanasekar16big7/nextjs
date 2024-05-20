d9374c952093a10437bc26e11adfd6ce5192a4af
GowthamS22 <gowtham@solution22.com.au>
2024-05-20 11:52:40 +0530

    profile path

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -55,15 +55,17 @@ export async function updateSession(request: NextRequest) {
   )
 
   const { data: { user } } = await supabase.auth.getUser();
-// console.log("user1 ", user);
-  const publicRoutes = ['/login', `/${request.nextUrl.pathname}/profile`, '/bussiness-card'];
-//   console.log("redirect to ", request.nextUrl.pathname)
+
+  let profilePath;
+  if(request.nextUrl.pathname.endsWith('/profile')) {
+    profilePath = request.nextUrl.pathname
+  }
+
+  const publicRoutes = ['/login', `${profilePath}`];
 
   if (!user && !publicRoutes.includes(request.nextUrl.pathname)) {
-    console.log(request.url, 'request.url')
     return NextResponse.redirect(new URL('/login', request.url));
   }
-  
   
   return response;
 }
