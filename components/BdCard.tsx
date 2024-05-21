205e39ad25fb3c29bfbbd2c90ebe35c1d6673013
Dhanasekar <Dhanasekar16big7>
2024-05-21 08:23:48 +0530

    name changes

--- components/BdCard.tsx
+++ components/BdCard.tsx
@@ -96,7 +96,7 @@ const BdCard: React.FC<Props> = ({ userId }) => {
     const vcard = `BEGIN:VCARD
 VERSION:3.0
 FN:${user.first_name} ${user.last_name}
-N:${user.first_name};${user.last_name}
+LN:${user.first_name} ${user.last_name}
 ORG:${user.company}
 TITLE:${user.designation}
 TEL;TYPE=WORK,VOICE:${user.mobile}
